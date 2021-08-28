5456c7ec086921510615082f4ccb123796042000
Simon Larsén <slarse@slar.se>
2021-08-29 00:36:59 +0200

    [feat] Check that indentation is consistent

--- src/yamlike.py
+++ src/yamlike.py
@@ -4,10 +4,12 @@ import sys
 
 import dataclasses
 
-from typing import Union, List
+from typing import Union, List, Optional
 
 _LINE_PATTERN = re.compile(r"^\w+:")
 _KEY_DELIMITER = ":"
+
+COMMENT_CHAR = "#"
 
 
 @dataclasses.dataclass(frozen=True)
@@ -21,17 +23,21 @@ class Node:
     indent: int
     parent: Union["Node", Root]
     children: List[Union["Node", str]]
+    line_nr: int
+
+
+class YamlikeError(RuntimeError):
+    pass
 
 
 def parse(text: str) -> dict:
     root = Root(children=[])
     parent: Union["Node", Root] = root
 
-    for raw_line in text.strip().split("\n"):
+    for line_nr, raw_line in enumerate(text.strip().split("\n"), start=1):
         line = _remove_comments(raw_line)
         if not line:
             continue
-
 
         indent = count_indent(line)
 
@@ -40,7 +46,7 @@ def parse(text: str) -> dict:
 
         key, rest = line.strip().split(_KEY_DELIMITER)
         children = [] if not rest else [rest.strip()]
-        node = Node(key, indent, parent, children)
+        node = Node(key, indent, parent, children, line_nr)
         parent.children.append(node)
 
         if not children:
@@ -50,10 +56,11 @@ def parse(text: str) -> dict:
 
 
 def _remove_comments(line: str) -> str:
-    comment_start_idx = line.find("#")
+    comment_start_idx = line.find(COMMENT_CHAR)
     if _is_comment_hash_at(line, comment_start_idx):
         return line[:comment_start_idx].rstrip()
     return line
+
 
 def _is_comment_hash_at(line: str, idx: int) -> bool:
     return idx == 0 or idx > 0 and line[idx - 1].isspace()
@@ -64,12 +71,27 @@ def _to_dict(root: Root) -> dict:
 
 
 def _children_to_dict(children: List[Union[str, Node]]) -> Union[dict, str]:
+    _check_consistent_indent([child for child in children if isinstance(child, Node)])
     first, *_ = children
     if isinstance(first, str):
         return first
     else:
         assert all(isinstance(child, Node) for child in children)
         return {child.key: _children_to_dict(child.children) for child in children}
+
+
+def _check_consistent_indent(nodes: List[Node]) -> None:
+    if not nodes:
+        return
+
+    expected_indent = nodes[0].indent
+    for node in nodes[1:]:
+        if node.indent != expected_indent:
+            raise YamlikeError(
+                f"Line {node.line_nr}: bad indentation, "
+                f"expected {expected_indent} "
+                f"but was {node.indent}"
+            )
 
 
 def count_indent(line: str) -> int:
--- tests/test_yamlike.py
+++ tests/test_yamlike.py
@@ -7,6 +7,7 @@ import yamlike
 
 RESOURCES = pathlib.Path(__file__).parent / "resources"
 VALID_INPUTS = RESOURCES / "valid"
+INVALID_INPUTS = RESOURCES / "invalid"
 ENCODING = "utf8"
 
 
@@ -26,3 +27,27 @@ def test_parses_valid_input_like_pyyaml(input_file: pathlib.Path):
     pyyamled = yaml.load(raw, Loader=yaml.SafeLoader)
 
     assert yamliked == pyyamled
+
+
+def _find_invalid_inputs():
+    return [
+        file
+        for file in INVALID_INPUTS.iterdir()
+        if file.is_file() and file.suffix == ".yml"
+    ]
+
+
+@pytest.mark.parametrize("input_file", _find_invalid_inputs())
+def test_raises_on_invalid_input(input_file: pathlib.Path):
+    raw = input_file.read_text(encoding=ENCODING)
+    first_line, *_ = raw.split("\n")
+    expected_error_msg = _parse_expected_error_message(first_line)
+
+    with pytest.raises(yamlike.YamlikeError) as exc_info:
+        yamlike.parse(raw)
+
+    assert expected_error_msg in str(exc_info.value)
+
+
+def _parse_expected_error_message(line: str) -> str:
+    return line[line.index(yamlike.COMMENT_CHAR) + 1 :].strip()
