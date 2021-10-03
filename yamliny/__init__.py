3b07c84e9ad4fafa0e4805ffa103743bd8901c60
Simon Larsén <slarse@kth.se>
2021-10-03 17:31:27 +0200

    [feat] Add dumps function (#3)

--- yamliny/__init__.py
+++ yamliny/__init__.py
@@ -1,3 +1,3 @@
-from yamliny._yamliny import loads, YamlinyError
+from yamliny._yamliny import loads, dumps, YamlinyError
 
-__all__ = ["loads", "YamlinyError"]
+__all__ = ["loads", "dumps", "YamlinyError"]
